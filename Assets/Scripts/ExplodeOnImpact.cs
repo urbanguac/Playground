16605e4cd606a18bc5f483c5fe3085d60bbf3654
Priansh Shah <priansh123@me.com>
2015-06-14 17:58:35 -0400

    Explosives, Spiders, and an Axe were added

---
+++ Assets/Scripts/ExplodeOnImpact.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnImpact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Collided");
+		if (other.gameObject.tag == "Weapon") 
+		{
+			gameObject.SendMessage("Explode");
+			Destroy(gameObject);
+			Debug.Log ("Explode message sent.");
+		}
+
+	}
+}
